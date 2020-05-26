f0bfbfc2b3a0482c10c8b63fa87c6c1fd2eb77d1
Kyle Watson <kyle.watson@wundermanthompson.com>
2020-05-26 11:17:55 -0400

    Initial Commit

---
+++ BracketExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BracketExample
+//
+//  Created by Kyle Watson on 5/26/20.
+//  Copyright © 2020 Kyle Watson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
